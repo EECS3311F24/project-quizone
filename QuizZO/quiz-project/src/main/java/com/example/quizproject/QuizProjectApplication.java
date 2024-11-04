2a0206a0e8ac2006b5f04e83f7853dbb3f48aff2
Karine <karine.davtyand@gmail.com>
2024-11-03 21:38:08 -0500

    Initial commit with full project files

---
+++ QuizZO/quiz-project/src/main/java/com/example/quizproject/QuizProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizProjectApplication.class, args);
+	}
+
+}
